971de7c6e4759503c8d936f3f4afe0437065c61a
davijr <davioliveiraj@hotmail.com>
2022-07-02 20:52:59 -0300

    - Add static (harded code) filter to menu items.

--- src/services/EditionService.ts
+++ src/services/EditionService.ts
@@ -3,6 +3,7 @@ import logger from '@config/logger'
 import { ModelUtils } from '@models/utils/ModelUtils'
 import RequestModel from '@models/utils/RequestModel'
 import ResponseModel from '@models/utils/ResponseModel'
+import { AppUtils } from '@utils/AppUtils'
 
 export class EditionService {
   async find (requestModel: RequestModel): Promise<ResponseModel> {
@@ -88,10 +89,16 @@ export class EditionService {
     }
   }
 
+  /**
+   * Purpose: To list menu options for EDITION module.
+   * Filter: Just show items according to business rule given by client.
+   * @returns menu items
+   */
   async getMenuOptions () {
     const modelKeys = Object.keys(database.models)
     const groups: string[] = []
-    const items: any[] = []
+    const menuItems: any[] = []
+    // create groups
     modelKeys.forEach(item => {
       const aux = item.replace(/([a-z])([A-Z])/g, '$1 $2')
       const groupName = aux.split(' ')[0]
@@ -99,27 +106,43 @@ export class EditionService {
         groups.push(groupName)
       }
     })
+    // assign items to group
     groups.forEach(group => {
       const newItem: any = {
         name: group,
-        active: true
+        active: true,
+        items: []
       }
-      newItem.items = []
       modelKeys.forEach(item => {
         if (item.startsWith(group)) {
-          newItem.items.push({
-            name: ModelUtils.getMenuItemName(item),
-            route: 'edition',
-            param: item,
-            active: true
+          const itemName = ModelUtils.getMenuItemName(item)
+          const menuItemsToFilter = AppUtils.menuItemsToFilter()
+          let belongsFilterValidation = false
+          menuItemsToFilter.forEach(i => {
+            const itemNameFilter = i.replace(/_/g, '').replace(/ /g, '').toUpperCase()
+            const itemNameCleared = item.replace(/ /g, '').toUpperCase()
+            if (itemNameFilter === itemNameCleared) {
+              belongsFilterValidation = true
+              return true
+            }
           })
+          if (belongsFilterValidation) {
+            newItem.items.push({
+              name: itemName,
+              route: 'edition',
+              param: item,
+              active: true
+            })
+          }
         }
       })
       ModelUtils.sort(newItem.items, 'name')
-      items.push(newItem)
+      if (newItem.items.length > 0) {
+        menuItems.push(newItem)
+      }
     })
-    ModelUtils.sort(items, 'name')
-    return items
+    ModelUtils.sort(menuItems, 'name')
+    return menuItems
   }
 
   async getAttributes (modelName: string) {
